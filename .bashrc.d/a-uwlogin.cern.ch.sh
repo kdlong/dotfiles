36e5ff98dbc380c06ad2a40ca574b2a8724ec264
Kenneth Long <kdlong@hep.wisc.edu>
2015-06-23 09:40:01 +0200

    Finally figured out how to go to beging of line in tmux

--- .bashrc.d/a-uwlogin.cern.ch.sh
+++ .bashrc.d/a-uwlogin.cern.ch.sh
@@ -10,11 +10,12 @@ if touch a 2>/dev/null; then
   rm a
 else
   echo "AFS Credentials expired; running kinit && aklog"
-  kinit -l 30d kelong@cern.ch && aklog
+  kinit -l 30d kelong@CERN.CH && aklog
 fi
 
 # Make git stop searching before hitting /afs networked directory
 export GIT_CEILING_DIRECTORIES=/afs/cern.ch
+. /data/cms/setup/bashrc
 
 # Aliases
 alias procrastinate="watch -n 30 'condor_q kdlong | tail -n 40'"
