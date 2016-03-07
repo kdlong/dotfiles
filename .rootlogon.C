1041a91a4100d9b4a5fc39f2679e96c6286c9996
Kenneth Long <kdlong@hep.wisc.edu>
2016-03-07 03:38:53 -0600

    Update autolibraryloader

--- .rootlogon.C
+++ .rootlogon.C
@@ -1,9 +1,15 @@
 {
-  if ( gSystem->Getenv("CMSSW_BASE") != 0 ) {
+  std::string cmssw_path = gSystem->Getenv("CMSSW_BASE");
+  if ( cmssw_path != 0) {
     // Provides access to methods in CMSSW framework
     gSystem->Load("libFWCoreFWLite.so");
-    AutoLibraryLoader::enable();
+    int cmssw_version = cmssw_path[cmssw_path.find("CMSSW_") + 6] - '0';
+    if ( cmssw_version < 7 )
+      AutoLibraryLoader::enable();
+    else
+      FWLiteEnabler::enable();
     gSystem->Load("libDataFormatsFWLite.so");
+    }
   }
 
   // Load dotroot
